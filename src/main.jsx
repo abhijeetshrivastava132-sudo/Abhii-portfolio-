92eb48cc3c2a182f52029b6abdfe1745361ae75d
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-05-29 15:13:16 +0530

    Add React main entry

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/reset.css';
+import './styles/tokens.css';
+import './styles/globals.css';
+
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App />);
