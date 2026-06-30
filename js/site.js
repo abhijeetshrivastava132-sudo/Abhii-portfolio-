fa978352e538be44dd4f2834effdd04ea76ceba6
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-06-30 09:44:21 +0545

    Clean index html from URL

--- js/site.js
+++ js/site.js
@@ -1,3 +1,8 @@
+if(window.location.pathname.endsWith('/index.html')){
+    const cleanPath = window.location.pathname.replace(/index\.html$/, '');
+    window.history.replaceState(null, '', cleanPath + window.location.search + window.location.hash);
+}
+
 document.addEventListener('contextmenu', function(event){
     event.preventDefault();
 });
